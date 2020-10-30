c0a3f857b122f718ba4dedd437d206fb4ec3c980
Itamar Gomes <itamargomes038@hotmail.com>
2020-10-30 00:13:17 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -39,6 +39,7 @@ self.addEventListener('fetch', function (event) {
   );
 });*/
 var CACHE_NAME = 'my-site-cache-v1';
+let deferredPrompt;
 var urlsToCache = [
   '/index.html',
   '/img/icones/palha-512.png',
@@ -57,4 +58,20 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+ });
 
